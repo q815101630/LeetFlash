297794ed8bb78bb251cee0b6770fdc196218774a
Lucheng_Hugh <hughqing@qq.com>
2022-04-24 18:47:41 -0500

    fix: tutorial page redirect to tutorial for now

--- client/src/pages/tutorial.tsx
+++ client/src/pages/tutorial.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 const TutorialPage = () => {
+  // open to google.com with useEffect
+
+  useEffect(() => {
+    window.open("https://lucheng.xyz/2022/04/14/leetflash/", "_blank");
+
+    //redirect to targetUrl
+    window.location.href = "https://leetflash.com";
+  }, []);
+
   return <div> Tutorial Page </div>;
 };
 
